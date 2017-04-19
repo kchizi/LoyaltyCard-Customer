cbffbba95e3583dd0a99455fc7bdccff337e7c5a
Sam <sponge3k@gmail.com>
2017-04-19 15:57:53 +1200

    Added FirebaseUI-Auth to CustomerLandingActivity
    Sign up and sign in screens implemented. Google sign in included - this requires activation in Firebase console before use.

--- app/src/main/java/card/loyalty/loyaltycardcustomer/CustomerLandingActivity.java
+++ app/src/main/java/card/loyalty/loyaltycardcustomer/CustomerLandingActivity.java
@@ -1,20 +1,37 @@
 package card.loyalty.loyaltycardcustomer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class CustomerLandingActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "CustomerLandingActivity";
+    private static final int RC_SIGN_IN = 123;
+
+    // Firebase Authentication Variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,9 @@ public class CustomerLandingActivity extends AppCompatActivity
         setContentView(R.layout.activity_customer_landing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Firebase Authentication Initialisation
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +60,63 @@ public class CustomerLandingActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Firebase UI Authentication
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged: is signed_in:" + user.getUid());
+                    onSignedInInitialise(user);
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged: is signed_out");
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    //.setTheme(R.style.AuthTheme) //set a theme for Firebase UI here
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    // Handling the Firebase UI Auth result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Sign in successful", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    // On resuming activity
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Add the firebase auth state listener
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    // On pausing activity
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Remove the firebase auth state listener
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 
     @Override
@@ -97,5 +174,15 @@ public class CustomerLandingActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // Once signed in initialise QR code and set any other user specific data here. For example attach database event listener here
+    private void onSignedInInitialise(FirebaseUser user){
+
+    }
+
+    // On signing out clean up any user specific data here. For example detatch database event listener
+    private void onSignedOutCleanup() {
+
     }
 }
