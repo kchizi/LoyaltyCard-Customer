b714d46790f55daf088d97a35cc100fc555e6bed
Unknown <meediyoka@gmail.com>
2017-06-10 23:48:43 +1200

    Add test for storage upload and download
    To be added to final merge

---
+++ app/src/main/java/card/loyalty/loyaltycardcustomer/tests/TestC.java
@@ -0,0 +1,19 @@
+package card.loyalty.loyaltycardcustomer.tests;
+
+
+/**
+ * Created by Thoma on 10/06/2017.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import card.loyalty.loyaltycardcustomer.TestsActivity;
+
+public class TestC {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(TestsActivity.mTestC_Passed);
+    }
+}
